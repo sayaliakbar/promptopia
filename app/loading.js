5455254abf14396306af35d9c355f5631982a736
Ali Akbar <sayaliakbar@gmail.com>
2025-03-15 01:55:05 +0500

    feat: add Loading component with spinner for improved user experience

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
+
+export default function Loading() {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <LoadingSpinner />
+    </div>
+  );
+}
