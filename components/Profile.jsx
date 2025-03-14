b59969db15cc5a50f4d8ff103952fbc55fe8d86d
Ali Akbar <sayaliakbar@gmail.com>
2025-03-15 00:10:23 +0500

    fix: update Profile component to display personalized messages based on user session

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,12 +1,21 @@
 import PromptCard from "./PromptCard";
+import { useSession } from "next-auth/react";
 
-const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+const Profile = ({ userId, data, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{name} Profile</span>
+        <span className="blue_gradient">
+          {userId === session?.user.id ? "My" : "User"} Profile
+        </span>
       </h1>
-      <p className="desc text-left">{desc}</p>
+      <p className="desc text-left">
+        {userId === session?.user.id
+          ? "Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+          : "Welcome to this user's profile page. Explore their exceptional prompts and get inspired by their creativity"}
+      </p>
       <div className="mt-16 prompt_layout">
         {data.map((post) => (
           <PromptCard
