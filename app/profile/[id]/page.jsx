53a799ef1f508eb9cf5976a141e8d6f893d97cc8
Ali Akbar <sayaliakbar@gmail.com>
2025-03-15 01:55:23 +0500

    feat: enhance MyProfile component with loading spinner and user data display

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,28 +1,37 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 import { useRouter, useParams } from "next/navigation";
-
-import Profile from "@components/Profile";
+import PromptCard from "@/components/PromptCard";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 const MyProfile = () => {
-  const params = useParams();
-  const userId = params.id;
-
+  const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState("");
   const router = useRouter();
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${userId}/posts`);
-      const data = await response.json();
+      setIsLoading(true);
+      try {
+        const response = await fetch(`/api/users/${id}/posts`);
+        const data = await response.json();
 
-      setPosts(data);
+        setPosts(data);
+        setUser(data?.[0].creator);
+      } catch (error) {
+        console.log("Error fetching user posts:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
-    if (userId) fetchPosts();
-  }, [userId]);
+    fetchPosts();
+  }, []);
 
   const handleEdit = (post) => {
     router.push(`/update-prompt/?id=${post._id}`);
@@ -54,12 +63,37 @@ const MyProfile = () => {
   };
 
   return (
-    <Profile
-      userId={userId}
-      data={posts}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
-    />
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">
+          {id === session?.user.id
+            ? "My Profile"
+            : `${user.username}'s Profile`}
+        </span>
+      </h1>
+      <p className="desc text-left">
+        {id === session?.user.id
+          ? "Welcome to your personalized profile page. View and manage your prompts."
+          : `Explore ${user.username}'s collection of AI prompts and creative ideas.`}
+      </p>
+
+      {isLoading ? (
+        <div className="mt-10 flex-center">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <div className="mt-10 prompt_layout">
+          {posts.map((post) => (
+            <PromptCard
+              key={post._id}
+              post={post}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+            />
+          ))}
+        </div>
+      )}
+    </section>
   );
 };
 
