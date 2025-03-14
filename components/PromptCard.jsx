cb68c364c1b745afe39607881ae40b7b73f31de3
Ali Akbar <sayaliakbar@gmail.com>
2025-03-15 00:10:32 +0500

    fix: update PromptCard to link creator's username to profile and adjust edit/delete visibility logic

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -34,20 +34,12 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             />
           </Link>
           <div className="flex flex-col">
-            <h3
-              className="font-satoshi font-semibold text-gray-900 w-fit cursor-pointer"
-              onClick={() => {
-                console.log(post.creator);
-              }}
-            >
-              {post.creator.username}
-            </h3>
-            <p
-              className="font-inter text-sm text-gray-500 cursor-pointer"
-              onClick={() => {
-                console.log(post.creator);
-              }}
-            >
+            <Link href={`/profile/${post.creator._id}`}>
+              <h3 className="font-satoshi font-semibold text-gray-900 w-fit cursor-pointer">
+                {post.creator.username}
+              </h3>
+            </Link>
+            <p className="font-inter text-sm text-gray-500 ">
               {post.creator.email}
             </p>
           </div>
@@ -74,22 +66,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         {post.tag}
       </p>
 
-      {session?.user.id === post.creator._id && pathname === "/profile" && (
-        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-          <p
-            className="font-inter text-sm green_gradient cursor-pointer"
-            onClick={handleEdit}
-          >
-            Edit
-          </p>
-          <p
-            className="font-inter text-sm orange_gradient cursor-pointer"
-            onClick={handleDelete}
-          >
-            Delete
-          </p>
-        </div>
-      )}
+      {session?.user.id === post.creator._id &&
+        pathname === `/profile/${session?.user.id}` && (
+          <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            <p
+              className="font-inter text-sm green_gradient cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className="font-inter text-sm orange_gradient cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )}
     </div>
   );
 };
