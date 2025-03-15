304d3c345b544eea0abb052a7e5c59502664cfae
Ali Akbar <sayaliakbar@gmail.com>
2025-03-15 05:32:46 +0500

    refactor: update GoogleProvider and NextAuth initialization to use default exports

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ import User from "@models/user";
 
 export const authOptions = {
   providers: [
-    GoogleProvider({
+    GoogleProvider.default({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
@@ -49,6 +49,6 @@ export const authOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth.default(authOptions);
 
 export { handler as GET, handler as POST };
